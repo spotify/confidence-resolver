d70e1aa1ac33b7b5048256c230eb78198805b867
Andreas Karlsson <andreask@spotify.com>
2026-02-13 15:49:25 +0100

    update js provider

--- openfeature-provider/js/src/LocalResolver.ts
+++ openfeature-provider/js/src/LocalResolver.ts
@@ -1,9 +1,9 @@
-import type { ResolveWithStickyRequest, ResolveWithStickyResponse } from './proto/confidence/wasm/wasm_api';
+import type { ResolveProcessRequest, ResolveProcessResponse } from './proto/confidence/wasm/wasm_api';
 import type { SetResolverStateRequest } from './proto/confidence/wasm/messages';
 import type { ApplyFlagsRequest } from './proto/confidence/flags/resolver/v1/api';
 
 export interface LocalResolver {
-  resolveWithSticky(request: ResolveWithStickyRequest): ResolveWithStickyResponse;
+  resolveProcess(request: ResolveProcessRequest): ResolveProcessResponse;
   setResolverState(request: SetResolverStateRequest): void;
   flushLogs(): Uint8Array;
   flushAssigned(): Uint8Array;
